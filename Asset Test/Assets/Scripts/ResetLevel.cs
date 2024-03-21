942049e0a95d28e7ff3d276374b0fd66c31ac627
ScottHufschmidt <scottedward.huf@gmail.com>
2024-03-20 18:08:59 -0700

    Added reset button to Pause menu

---
+++ Asset Test/Assets/Scripts/ResetLevel.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class RestartLevel : MonoBehaviour
+{
+    private int current_level;
+    [SerializeField] Button resetButton;
+    void Start()
+    {
+        if (PlayerController.PlayerInstance == null)
+        {
+            current_level = 1;
+        }
+        else
+            current_level = PlayerController.PlayerInstance.currLevel;
+        resetButton.onClick.AddListener(Reset);
+    }
+
+    // Update is called once per frame
+    void Reset()
+    {
+        SceneManager.LoadScene("Level " + current_level);
+        Time.timeScale = 1;
+    }
+}
